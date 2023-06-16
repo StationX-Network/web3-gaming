fb7db6d1119fae2bee3140b0621968139d5ac1ea
bhavya2611 <bhavya.y.mehta@gmail.com>
2023-06-16 16:41:35 +0800

    minor changes

--- src/App.js
+++ src/App.js
@@ -58,7 +58,16 @@ function App() {
           />
           <div className='listing-div'>
             {selected === "Claim" ? (
-              <> {isOwner ? <ClaimSetupCard /> : <ClaimRewardCard />} </>
+              <div
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  display: "flex",
+                  justifyContent: "center"
+                }}
+              >
+                {isOwner ? <ClaimSetupCard /> : <ClaimRewardCard />}{" "}
+              </div>
             ) : (
               <>
                 {!isOwner ? (
